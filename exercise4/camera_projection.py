bf3eef8ab538e887f0c90656286dc090f0809716
Eirik Fagerhaug <eirik.fagerhaug@gmail.com>
2018-06-29 17:51:09 +0200

    small changes

--- exercise4/camera_projection.py
+++ exercise4/camera_projection.py
@@ -4,18 +4,16 @@ from pathlib import Path
 import cv2
 
 
-intrinsics = np.array([
+intrinsics2 = np.array([
     [585.5644470303048, 0, 630.5897331003082],
     [0, 584.8964075434083, 477.7496476463654],
     [0, 0, 1]])
 
-intrinsics2 = np.array([[585.2304272868565, 0.0, 1190.0], [0.0, 585.2304272868565, 640.5], [0.0, 0.0, 1.0]])
+intrinsics = np.array([[585.2304272868565, 0.0, 1190.0], [0.0, 585.2304272868565, 640.5], [0.0, 0.0, 1.0]])
 
-extrinsics = np.array([[-0.11350664, -0.04971457, -0.99229265, 0.16266196],
-                       [0.99340159, -0.02218241, -0.11252213, -1.61062595],
-                       [-0.01641745, -0.9985171, 0.05190438, 0.20266677]])
-
-width, height = 1280, 960
+transform = np.array([[-0.11350664, -0.04971457, -0.99229265, 0.16266196],
+                      [0.99340159, -0.02218241, -0.11252213, -1.61062595],
+                      [-0.01641745, -0.9985171, 0.05190438, 0.20266677]])
 
 
 def load_polygons(path):
@@ -27,23 +25,27 @@ def load_polygons(path):
     return polygons
 
 
-def process_image(img, polygons, intr, extr):
-    transform = intr @ extr
+def process_image(img, polygons, intr, transform):
+    # transform = intr @ extr
     print(transform)
     out = img.copy()
     for polygon in polygons:
         projected = []
         # below can probably be vectorized somehow, maybe consider using bboxes as well
         for i, r in polygon.iterrows():
-            coords = np.array([[r.x], [r.y], [r.z], [1]])
+            coords = np.array([[0], [0], [0], [1]])# np.array([[r.x], [r.y], [r.z], [1]])
             coords = transform @ coords
+            print(coords)
+            coords = intrinsics @ coords
             print(coords)
             coords = [[(coords[0][0])/(coords[2][0]), (coords[1][0])/(coords[2][0])]]  #
             coords = np.round(coords).astype(np.int32)
             print(coords)
             projected.append(coords)
+            break
         projected = np.array(projected)
         cv2.fillPoly(img, [projected], (0, 255, 0))
+        break
     cv2.addWeighted(img, 0.7, out, 0.3, 0, out)
     return out
 
@@ -56,9 +58,10 @@ def main():
     polygons = load_polygons('polygons')
     image = cv2.imread('images/car02-frame000258.png')
     # -0.687271983, 0.014578437, 0.005606003, 0.726232238
-    img = process_image(image, polygons, intrinsics, extrinsics)
+    img = process_image(image, polygons, intrinsics, transform)
+    print(img.shape)
     cv2.imshow('img', img)
-
+    cv2.imwrite('test.jpg', img)
 
 if __name__ == '__main__':
     main()
