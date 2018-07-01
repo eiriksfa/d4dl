9257939271ec4bcb80ec760d19b9b3861e79f971
Eirik Fagerhaug <eirik.fagerhaug@gmail.com>
2018-07-01 17:52:59 +0200

    small test

--- exercise4/camera_projection.py
+++ exercise4/camera_projection.py
@@ -3,17 +3,15 @@ import pandas as pd
 from pathlib import Path
 import cv2
 
-
-intrinsics2 = np.array([
-    [585.5644470303048, 0, 630.5897331003082],
-    [0, 584.8964075434083, 477.7496476463654],
-    [0, 0, 1]])
-
 intrinsics = np.array([[585.2304272868565, 0.0, 1190.0], [0.0, 585.2304272868565, 640.5], [0.0, 0.0, 1.0]])
 
 transform = np.array([[-0.11350664, -0.04971457, -0.99229265, 0.16266196],
                       [0.99340159, -0.02218241, -0.11252213, -1.61062595],
                       [-0.01641745, -0.9985171, 0.05190438, 0.20266677]])
+
+transform = np.array([[-9.88373548e-01, -1.12874824e-03, 1.52040968e-01, 3.55766200e+00],
+                        [-1.52045131e-01, 7.93392204e-03, -9.88341708e-01, -1.10985354e+00],
+                        [-9.06922267e-05, -9.99967889e-01, -8.01329935e-03, 1.72194294e-01]])
 
 
 def load_polygons(path):
@@ -29,22 +27,37 @@ def process_image(img, polygons, intr, transform):
     # transform = intr @ extr
     print(transform)
     out = img.copy()
-    for polygon in polygons:
-        projected = []
-        # below can probably be vectorized somehow, maybe consider using bboxes as well
-        for i, r in polygon.iterrows():
-            coords = np.array([[0], [0], [0], [1]])# np.array([[r.x], [r.y], [r.z], [1]])
-            coords = transform @ coords
-            print(coords)
-            coords = intrinsics @ coords
-            print(coords)
-            coords = [[(coords[0][0])/(coords[2][0]), (coords[1][0])/(coords[2][0])]]  #
-            coords = np.round(coords).astype(np.int32)
-            print(coords)
-            projected.append(coords)
-        projected = np.array(projected)
-        cv2.fillPoly(img, [projected], (0, 255, 0))
-    cv2.addWeighted(img, 0.7, out, 0.3, 0, out)
+    coords = np.array([[3.805], [-2.333], [-0.027], [1]])
+    print(coords)
+    coords1 = transform @ coords
+    print('========')
+    print(coords1)
+    coords11 = intrinsics @ coords1
+    print('========')
+    coords11 = [[(coords11[0][0])/(coords11[2][0]), (coords11[1][0])/(coords11[2][0])]]
+    coords11 = np.round(coords11).astype(np.int32)
+    print(coords11)
+    # for polygon in polygons:
+    #     projected = []
+    #     # below can probably be vectorized somehow, maybe consider using bboxes as well
+    #     for i, r in polygon.iterrows():
+    #         coords = np.array([[r.x], [r.y], [r.z], [1]])
+    #         coords = transform @ coords
+    #         print(coords)
+    #         coords = intrinsics @ coords
+    #         print(coords)
+    #         coords = [[(coords[0][0])/(coords[2][0]), (coords[1][0])/(coords[2][0])]]  #
+    #         coords = np.round(coords).astype(np.int32)
+    #         print(coords)
+    #         projected.append(coords)
+    #     projected = np.array(projected)
+    #     cv2.fillPoly(img, [projected], (0, 255, 0))
+    # cv2.addWeighted(img, 0.7, out, 0.3, 0, out)
+    print(out.shape)
+    # cv2.circle(out, (coords11[0][0], 1281-(coords11[0][1])), 40, (255, 0, 0), thickness=7)
+    test = (coords11[0][0], coords11[0][1])
+    print(test)
+    cv2.circle(out, (1142, 750), 20, (255, 0, 0), thickness=10)
     return out
 
 
@@ -54,12 +67,12 @@ def process_csv(csv, polygons, intr):
 
 def main():
     polygons = load_polygons('polygons')
-    image = cv2.imread('images/car02-frame000258.png')
+    image = cv2.imread('images/car02-frame000175.png')
     # -0.687271983, 0.014578437, 0.005606003, 0.726232238
     img = process_image(image, polygons, intrinsics, transform)
-    print(img.shape)
     cv2.imshow('img', img)
     cv2.imwrite('test.jpg', img)
+
 
 if __name__ == '__main__':
     main()
--- exercise4/carcapture.py
+++ exercise4/carcapture.py
@@ -14,127 +14,142 @@ import signal
 import image_geometry
 from pathlib import Path
 
-#rosrun image_transport republish compreed in:=output/image raw out:=camera_out/image
+
+# rosrun image_transport republish compreed in:=output/image raw out:=camera_out/image
+
+intrinsics = np.array([[585.2304272868565, 0.0, 1190.0], [0.0, 585.2304272868565, 640.5], [0.0, 0.0, 1.0]])
+
 
 def load_polygons(path='/home/novian/term2/dl4ad/repo2/d4dl/exercise4/polygons'):
-	p = Path(path)
-   	polygons = []
-   	for csv in [f for f in p.iterdir()]:
-   		df = pd.read_csv(csv)
-   		polygons.append(df)
-   	return polygons
- 
+    p = Path(path)
+    polygons = []
+    for csv in [f for f in p.iterdir()]:
+        df = pd.read_csv(csv)
+        polygons.append(df)
+    return polygons
+
+
 class CameraPose(object):
 
-	def __init__(self):
-		self.tf_listener = tf.TransformListener()
-		self.num = 0
-		self.broken_amt = 0
-		self.imgseq = []
-		self.timestamp = []
-		self.broken = []
-		self.mat = []
-		self.prop = None
-		self.polygon = load_polygons()
-		self.counter = 0
+    def __init__(self):
+        self.tf_listener = tf.TransformListener()
+        self.num = 0
+        self.broken_amt = 0
+        self.imgseq = []
+        self.timestamp = []
+        self.broken = []
+        self.mat = []
+        self.prop = None
+        self.polygon = load_polygons()
+        self.counter = 0
 
+    def callback_camerainfo(self, prop):
+        # print('got image')
+        print(prop.header.stamp)
+        print(rospy.Time.now())
+        self.prop = prop
 
+    def callback_camera(self, img):
+        namefile = '{}{:06d}{}'.format('car02-frame', self.num, '.png')
+        try:
+            print('got image')
+            self.num = self.num + 1
+            np_arr = np.fromstring(img.data, np.uint8)
+            image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+        except CvBridgeError as e:
+            print(e)
+        else:
+            # cv2.imshow("window",image_np)
+            # cv2.imwrite('test.jpg',image_np)
+            # print("saving car01-frame%06i.png" % self.num)
+            print("saving " + namefile)
+        # cv2.imwrite(os.path.join('/home/novian/catkin_ws/src/bagfile/car-02/', namefile), image_np)
+        mtr = []
+        trans = [0.0, 0.0, 0.0]
+        rot = [0.0, 0.0, 0.0, 0.0]
+        broken = False
+        try:
+            (trans, rot) = self.tf_listener.lookupTransform('/mocap', 'front_cam', img.header.stamp)
+        except ExtrapolationException as e:
+            print(e)
+            broken = True
+            self.broken_amt += 1
+        except ConnectivityException as e:
+            print(e)
+            broken = True
+            self.broken_amt += 1
+        else:
+            mtr = self.tf_listener.fromTranslationRotation(trans, rot)
 
-	def callback_camerainfo(self,prop):
-		#print('got image')
-		print(prop.header.stamp)
-		print(rospy.Time.now())
-		self.prop = prop
+        print(mtr)
+        # TODO: Convert mtr to a numpy matrix of form:
+        # 4x3 (drop last row, it should be [0, 0, 0, 1] and not necessary)
+        # np.array(mtr)...
 
-	def callback_camera(self,img):
-		namefile = '{}{:06d}{}'.format('car02-frame',self.num,'.png')
-		try:
-			print('got image')
-			self.num=self.num+1
-			np_arr = np.fromstring(img.data, np.uint8)
-			image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-		except CvBridgeError as e:
-			print(e)
-		else:
-			#cv2.imshow("window",image_np)
-			#cv2.imwrite('test.jpg',image_np)
-			#print("saving car01-frame%06i.png" % self.num)
-			print("saving "+namefile)
-			#cv2.imwrite(os.path.join('/home/novian/catkin_ws/src/bagfile/car-02/', namefile), image_np)
-		mtr = []
-		trans = [0.0,0.0,0.0]	
-		rot = [0.0,0.0,0.0,0.0]
-		broken = False
-		try:
-			(trans,rot) = self.tf_listener.lookupTransform('/mocap','front_cam',img.header.stamp)
-		except ExtrapolationException as e:
-			print(e)
-			broken=True
-			self.broken_amt+=1
-		except ConnectivityException as e:
-			print(e)
-			broken=True
-			self.broken_amt+=1
-		else:
-			mtr = self.tf_listener.fromTranslationRotation(trans,rot)
+        ig = image_geometry.PinholeCameraModel()
+        ig.fromCameraInfo(self.prop)
 
-		print(mtr)
+        out = image_np.copy()
+        # for polygon in self.polygon:
+        #     projected = []
+        #     # below can probably be vectorized somehow, maybe consider using bboxes as well
+        #     for i, r in polygon.iterrows():
+        #         coords = np.array([[r.x], [r.y], [r.z], [1]])
+        #         projected.append(coords)
+        #         coords = np.matmul(mtr, coords)
+        #         coord = np.matmul(intrinsics, coords)
+        #         # TODO: Try to get the below to work, check input type (numpy array, normal array?, shape etc), check if it gives the same coord as above..
+        #         #coord = ig.project3dToPixel(coords)
+        #         print(coord)
+        #     projected = np.array(projected)
+        #     cv2.fillPoly(image_np, [projected], (0, 255, 0))
+        # cv2.addWeighted(image_np, 0.7, out, 0.3, 0, out)
 
-		ig = image_geometry.PinholeCameraModel()
-		ig.fromCameraInfo(self.prop)
-		
+        coords = np.array([[3.805], [-2.333], [-0.027], [1]])
+        coords = np.matmul(mtr, coords)
+        coord = np.matmul(intrinsics, coords)
+        coord = [(coord[0][0]) / (coord[2][0]), (coord[1][0]) / (coord[2][0])]
+        coord = np.round(coord).astype(np.int32)
+        # TODO: Try to get the below to work, check input type (numpy array, normal array?, shape etc), check if it gives the same coord as above..
+        # (TODO: One function below should replace the three lines above, but it probably needs the original mtr, not the numpy array one with reduced dimensions)
+        #coord = ig.project3dToPixel(coords)
+        # TODO: Test around with the array below, see if you can get it to make sense?
+        cv2.circle(out, (coord[0], out.shape[0]-coord[1]), 10, (255, 0, 0), thickness=10)
+        cv2.circle(out, (coord[0], coord[1]), 10, (0, 255, 0), thickness=10)
+        print(coord)
+        namefile = str(self.counter) + '_out.png'
+        self.counter += 1
+        cv2.imwrite(os.path.join('/home/novian/catkin_ws/src/bagfile/car-02n/', namefile), out)
 
-		out = image_np.copy()
-		for polygon in self.polygon:
-			projected = []
-			# below can probably be vectorized somehow, maybe consider using bboxes as well
-			for i, r in polygon.iterrows():
-				coords = np.array([[r.x], [r.y], [r.z], [1]])
-				projected.append(coords)
-				coords = np.matmul(np.array(mtr) ,coords)
-				coord = ig.project3dToPixel(coords)
-				print(coord)
-			projected = np.array(projected)
-			cv2.fillPoly(image_np, [projected], (0, 255, 0))
-		cv2.addWeighted(image_np, 0.7, out, 0.3, 0, out)
-		namefile = str(self.counter) + '_out.png'
-		self.counter += 1
-		cv2.imwrite(os.path.join('/home/novian/catkin_ws/src/bagfile/car-02n/', namefile), out)
+        self.mat.append(mtr)
+        self.imgseq.append(namefile)
+        self.timestamp.append(img.header.stamp)
+        self.broken.append(broken)
+        print(trans)
+        print(rot)
+        # print(self.mat)
 
+        print(self.broken_amt)
 
+    def hook(self):
+        print('shutdown')
+        rawdata = {
+            'imgseq': self.imgseq,
+            'timestamp': self.timestamp,
+            'broken': self.broken,
+            'mat': self.mat}
+        df = pd.DataFrame(rawdata, columns=['imgseq', 'timestamp', 'broken', 'mat'])
 
+    # df.to_csv('car-01.csv')
 
-		self.mat.append(mtr)
-		self.imgseq.append(namefile)
-		self.timestamp.append(img.header.stamp)
-		self.broken.append(broken)
-		print(trans)
-		print(rot)
-		#print(self.mat)
-
-		print(self.broken_amt)
-
-	def hook(self):
-		print('shutdown')
-		rawdata = {
-			'imgseq':self.imgseq,
-			'timestamp':self.timestamp,
-			'broken':self.broken,
-			'mat':self.mat}
-		df = pd.DataFrame(rawdata,columns=['imgseq','timestamp','broken','mat'])
-		#df.to_csv('car-01.csv')
-
-	def listener(self):
-		rospy.init_node('listener',anonymous=True)
-		rospy.Subscriber("output/camera_info", CameraInfo, self.callback_camerainfo)
-		rospy.Subscriber("output/image/compressed", CompressedImage, self.callback_camera)
-		rospy.on_shutdown(self.hook)
-		rospy.spin()
+    def listener(self):
+        rospy.init_node('listener', anonymous=True)
+        rospy.Subscriber("output/camera_info", CameraInfo, self.callback_camerainfo)
+        rospy.Subscriber("output/image/compressed", CompressedImage, self.callback_camera)
+        rospy.on_shutdown(self.hook)
+        rospy.spin()
 
 
 if __name__ == '__main__':
-	cp = CameraPose()
-	cp.listener()
-
-
-
+    cp = CameraPose()
+    cp.listener()
