1a037e68e81c62c6d7d10284b61e4d1008beb4f8
nobinov <novianhabibie@gmail.com>
2018-07-03 20:37:49 +0200

    edit carcapture2.py - worked

--- exercise4/carcapture2.py
+++ exercise4/carcapture2.py
@@ -149,8 +149,7 @@ class CameraPose(object):
                 coords = np.array([[r.x], [r.y], [r.z], [1]])
                 c = np.matmul(P, coords)
                 coords = np.matmul(extrinsics, coords)
-
-                if coords[2][0] > 0 and (math.atan2(coords[1][0], coords[0][0]) > 0.3 or math.atan2(coords[1][0], coords[0][0]) < 0.3):  # Front of camera
+                if coords[2][0] > 0.00001:
                     #coords = np.matmul(trans, coords)
                     coords = np.matmul(self.intrinsics, coords)
                     coords = [(coords[0][0]) / (coords[2][0]), (coords[1][0]) / (coords[2][0])]
